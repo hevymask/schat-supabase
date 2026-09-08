89012d771d652c60c15ed59451598929abaf30c6
Hevy Mask <hevymask@gmail.com>
2026-09-08 20:57:19 +0900

    Add motion, shadcn, tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     babel({ presets: [reactCompilerPreset()] })
-  ]
+  ],
+
+  resolve: {
+    tsconfigPaths: true
+  }
 })
